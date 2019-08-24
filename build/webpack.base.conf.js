ee338d337678e4d4dbd0138d8adfb8a2ab012426
nan.chen <nan.chen@dena.com>
2019-08-24 17:46:47 +0800

    vasdvasdvasdv

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -14,7 +14,7 @@ const rules = require("./webpack.rules.conf.js");
 // 获取html-webpack-plugin参数的方法
 var getHtmlConfig = function (name, chunks) {
     return {
-        template: `./src/pages/${name}/index.html`, 
+        template: `./src/pages/${name}/index.html`,
         filename: process.env.NODE_ENV === "development"? `${name.slice(name.lastIndexOf('/') + 1)}.html`:`html/${name.slice(name.lastIndexOf('/') + 1)}.html`,
         inject: true,
         hash: false, //开启hash  ?[hash]
@@ -36,7 +36,7 @@ function getEntry() {
                 end = name.length - 3;
             var eArr = [];
             var n = name.slice(start, end);
-            n = n.slice(0, n.lastIndexOf('/')); //保存各个组件的入口 
+            n = n.slice(0, n.lastIndexOf('/')); //保存各个组件的入口
             n = n.split('pages/')[1];
             eArr.push(name);
             entry[n] = eArr;
@@ -65,7 +65,7 @@ module.exports = {
                 vendor: { // 抽离第三方插件
                     test: /node_modules/, // 指定是node_modules下的第三方包
                     chunks: 'initial',
-                    name: 'vendor', // 打包后的文件名，任意命名    
+                    name: 'vendor', // 打包后的文件名，任意命名
                     // 设置优先级，防止和自定义的公共代码提取时被覆盖，不进行打包
                     priority: 10
                 },
@@ -80,11 +80,11 @@ module.exports = {
     },
     plugins: [
         //静态资源输出
-        new copyWebpackPlugin([{
+        /*new copyWebpackPlugin([{
             from: path.resolve(__dirname, "../src/static"),
             to: './static',
             ignore: ['.*']
-        }]),
+        }]),*/
         // 消除冗余的css代码
         new purifyCssWebpack({
             paths: glob.sync(path.join(__dirname, "../src/pages/*/*.html"))
